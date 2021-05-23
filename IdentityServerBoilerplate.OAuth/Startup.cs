709babf983098fe9126e8a108c8d4ddfc641d39c
fyzact <feyyazacet@gmail.com>
2021-05-23 22:58:17 +0100

    added aspnetcore.mvc package

--- IdentityServerBoilerplate.OAuth/Startup.cs
+++ IdentityServerBoilerplate.OAuth/Startup.cs
@@ -26,7 +26,7 @@ namespace IdentityServerBoilerplate.OAuth
                 //.AddInMemoryIdentityResources(Config.IdentityResources)
                 .AddInMemoryApiScopes(Config.ApiScopes);
 
-
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
